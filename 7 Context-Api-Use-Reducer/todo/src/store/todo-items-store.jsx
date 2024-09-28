84df97413db853387aab14f65e4f7ffbb1331bcf
Hiten Jaypal <100457414+hitenjaypal@users.noreply.github.com>
2024-09-28 17:39:47 +0530

    Add files via upload

---
+++ 7 Context-Api-Use-Reducer/todo/src/store/todo-items-store.jsx
@@ -0,0 +1,73 @@
+import { useReducer } from "react";
+import { createContext } from "react";
+
+export const TodoItemsContext = createContext({
+  todoItems: [],
+  addNewItem: () => { },
+  deleteItem: () => { },
+});
+
+
+// todoitems
+const todoItemsReducer = (currTodoItems, action) => {
+  let newTodoItems = currTodoItems;
+
+  if (action.type === "NEW_ITEM") {
+    newTodoItems = [
+      ...currTodoItems,
+      { name: action.payload.itemName, dueDate: action.payload.itemDueDate },
+    ];
+
+  } else if (action.type === "DELETE_ITEM") {
+    newTodoItems = currTodoItems.filter(
+      (item) => item.name !== action.payload.itemName
+    );
+  }
+  return newTodoItems;
+};
+
+
+// context providers
+const TodoItemsContextProvider = ({ children }) => {
+  const [todoItems, dispatchTodoItems] = useReducer(todoItemsReducer, []);
+
+  //addNewItem
+  const addNewItem = (itemName, itemDueDate) => {
+    const newItemAction = {
+      type: "NEW_ITEM",
+      payload: {
+        itemName,
+        itemDueDate,
+      },
+    };
+    dispatchTodoItems(newItemAction);
+  };
+
+  //delItem
+  const deleteItem = (todoItemName) => {
+
+    const deleteItemAction = {
+      type: "DELETE_ITEM",
+      payload: {
+        itemName: todoItemName,
+      },
+    };
+    dispatchTodoItems(deleteItemAction);
+  };
+
+
+  // Return
+  return (
+    <TodoItemsContext.Provider
+      value={{
+        todoItems,
+        addNewItem,
+        deleteItem,
+      }}
+    >
+      {children}
+    </TodoItemsContext.Provider>
+  );
+};
+
+export default TodoItemsContextProvider;
